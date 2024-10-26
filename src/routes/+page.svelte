d32f5e7f8416a86d0701d9a39d87c0fbc16e125a
Lasse H. Bomholt <lasse@bomh.net>
2024-10-26 18:11:40 +0200

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<h1>Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation
+</p>
+
+<svg width="200" version="1.1" height="200" xmlns="http://www.w3.org/2000/svg">
+  <use href="/svg/king.svg#king"></use>
+</svg>
